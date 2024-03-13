fedb1ae955a98986e7acce342ac1715a30dc20a2
amehta7 <amimehta2811@gmail.com>
2024-03-13 14:45:55 -0700

    connect to MongoDB

--- server/index.js
+++ server/index.js
@@ -1,6 +1,15 @@
+import dotenv from 'dotenv'
 import express from 'express'
+import mongoose from 'mongoose'
+
+dotenv.config()
 
 const app = express()
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log('Connected to MongoDB!'))
+  .catch((error) => console.log(error))
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000!')
