f2c0d5a117b2aa647ca5dcc3c5a74b5e8d2bd732
amehta7 <amimehta2811@gmail.com>
2024-03-19 17:44:49 -0700

    add update user functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,12 +1,59 @@
-import { useSelector } from 'react-redux'
+import { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import {
+  updateUserFailure,
+  updateUserStart,
+  updateUserSuccess,
+} from '../features/user/userSlice'
 
 const Profile = () => {
-  const { currentUser } = useSelector((state) => state.user)
+  const { currentUser, loading, error } = useSelector((state) => state.user)
+
+  const [fromData, setFormData] = useState({})
+
+  const dispatch = useDispatch()
+
+  //console.log(fromData)
+
+  const handleChange = (e) => {
+    setFormData((prev) => {
+      return {
+        ...prev,
+        [e.target.id]: e.target.value,
+      }
+    })
+  }
+
+  const submitHandler = async (e) => {
+    e.preventDefault()
+    try {
+      dispatch(updateUserStart())
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+        method: 'POST',
+        headers: {
+          'Contente-Type': 'application/json',
+        },
+        body: JSON.stringify(FormData),
+      })
+
+      const data = await res.json()
+
+      //console.log(data)
+      if (data.success === false) {
+        dispatch(updateUserFailure(data.message))
+        return
+      }
+
+      dispatch(updateUserSuccess(data))
+    } catch (error) {
+      dispatch(updateUserFailure(error.message))
+    }
+  }
 
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
-      <form className='flex flex-col gap-4'>
+      <form onSubmit={submitHandler} className='flex flex-col gap-4'>
         <img
           className='w-24 h-24 rounded-full object-cover cursor-pointer self-center mt-2'
           src='https://media.istockphoto.com/id/1314335932/vector/user-avatar-profile-icon-black-vector-illustration-website-or-app-member-ui-button.jpg?s=612x612&w=0&k=20&c=HPIFqbxMi_eQygr02gXt7SULaU1lhd25githq9GtqMg='
@@ -17,22 +64,30 @@ const Profile = () => {
           type='text'
           id='username'
           placeholder='username'
+          defaultValue={currentUser.username}
           className='border p-3 rounded-lg'
+          onChange={handleChange}
         />
         <input
           type='email'
           id='email'
           placeholder='email'
+          defaultValue={currentUser.email}
           className='border p-3 rounded-lg'
+          onChange={handleChange}
         />
         <input
           type='password'
           id='password'
           placeholder='password'
           className='border p-3 rounded-lg'
+          onChange={handleChange}
         />
-        <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-90 disabled:opacity-80'>
-          update
+        <button
+          className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-90 disabled:opacity-80'
+          disabled={loading}
+        >
+          {loading ? 'Loading...' : 'Update'}
         </button>
         <button className='bg-green-700 text-white p-3 rounded-lg uppercase hover:opacity-90 disabled:opacity-80'>
           create listing
@@ -42,6 +97,7 @@ const Profile = () => {
         <span className='text-red-700 cursor-pointer'>Delete Account</span>
         <span className='text-red-700 cursor-pointer'>Sign Out</span>
       </div>
+      {error && <p className='text-red-500 mt-3'>{error}</p>}
     </div>
   )
 }
